ebf858c4556180b9187000858759adcce329cee3
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2020-01-03 13:53:24 +0000

    connection Redux to React... somewhat complex relationship, revise this

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './App';
-import reducer from '../store/reducer';
+import reducer from './store/reducer';
 import registerServiceWorker from './registerServiceWorker';
 
 const store = createStore(reducer);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
